81f4fb653b4e496259025097f055206f492d2fa1
Twixmixy <janet@twixmixy.com>
2012-12-06 16:09:09 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LFD::Application.config.secret_token = '34179511f0bb968a6d3f247e2f412d6a1dd4a64ec447a3cc21ad2e7ad3d5c3cdb5bc7358e21f9c6063bbf2cf84c2adeb4f8b373fd075ac4300c38ec307824fc4'
